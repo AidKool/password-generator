d47ab675dcd7b38bc1c332bb0aff4699b585cbdc
jordi <jordi.lanau@yahoo.com>
2021-10-30 18:42:50 +0100

    Implement function to get password length from user

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector('#password');
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword);
+
+function getPasswordLength() {
+  const minLength = 8;
+  const maxLength = 128;
+  let length = 0;
+  while (!(length >= minLength && length <= maxLength)) {
+    length = prompt('Enter password length.\nPasswords must be between 8 and 128 characters long.', 20);
+  }
+  return length;
+}
+
+
